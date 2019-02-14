bdaecd89fefaa71da2b0366ab513ca1f11efb2c8
mrrom <mr.roman.gray@gmail.com>
2019-02-14 21:56:51 +0600

    Create a Navigation Drawer Menu in the app

---
+++ app/src/main/java/com/mobilekck/KCKFragment.java
@@ -0,0 +1,18 @@
+package com.mobilekck;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class KCKFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_kck, container, false);
+    }
+}
--- app/src/main/java/com/mobilekck/MainActivity.java
+++ app/src/main/java/com/mobilekck/MainActivity.java
@@ -1,13 +1,78 @@
 package com.mobilekck;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new MainFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_main);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_main:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MainFragment()).commit();
+                break;
+            case R.id.nav_bills:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new BillsFragment()).commit();
+                break;
+            case R.id.nav_adboard:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AdBoardFragment()).commit();
+                break;
+            case R.id.nav_premium:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new PremiumFragment()).commit();
+                break;
+            case R.id.nav_kck:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new KCKFragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
