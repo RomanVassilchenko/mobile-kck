e296951081d4ba92f14fca03747c1d514493644f
mrrom <mr.roman.gray@gmail.com>
2019-02-27 23:01:06 +0600

    Create and add "InfoActivity"

--- app/src/main/java/com/mobilekck/MainActivity.java
+++ app/src/main/java/com/mobilekck/MainActivity.java
@@ -1,15 +1,15 @@
 package com.mobilekck;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
@@ -64,6 +64,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.nav_admin:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new AdminFragment()).commit();
+                break;
+            case R.id.nav_info:
+                Intent intent = new Intent(MainActivity.this, InfoActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.nav_buyer:
+                Intent intent2 = new Intent(MainActivity.this, ChoseActivity.class);
+                startActivity(intent2);
                 break;
         }
 
