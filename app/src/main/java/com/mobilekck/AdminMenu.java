05eab7999f407340762bfcab56b078497ac19097
mrrom <mr.roman.gray@gmail.com>
2019-03-25 16:57:07 +0600

    v1.04 - Add Update and Delete buttons in AdminMenu - Autocompletion when you change in AdminMenu

--- app/src/main/java/com/mobilekck/AdminMenu.java
+++ app/src/main/java/com/mobilekck/AdminMenu.java
@@ -12,8 +12,8 @@ import android.widget.EditText;
 
 public class AdminMenu extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnAdd, btnRead, btnClear;
-    EditText etIIN, etName, etSurName, etLastName, etAdress;
+    Button btnAdd, btnRead, btnClear, btnUpd, btnDel;
+    EditText etIIN, etName, etSurName, etLastName, etAdress, etId;
     DBHelper dbHelper;
 
     @Override
@@ -28,6 +28,12 @@ public class AdminMenu extends AppCompatActivity implements View.OnClickListener
         btnRead = findViewById(R.id.btnRead);
         btnRead.setOnClickListener(this);
 
+        btnUpd = (Button) findViewById(R.id.btnUpd);
+        btnUpd.setOnClickListener(this);
+
+        btnDel = (Button) findViewById(R.id.btnDel);
+        btnDel.setOnClickListener(this);
+
         btnClear = findViewById(R.id.btnClear);
         btnClear.setOnClickListener(this);
 
@@ -36,6 +42,7 @@ public class AdminMenu extends AppCompatActivity implements View.OnClickListener
         etSurName = findViewById(R.id.etSurname);
         etLastName = findViewById(R.id.etLastname);
         etAdress = findViewById(R.id.etAdress);
+        etId = (EditText) findViewById(R.id.etId);
 
         dbHelper = new DBHelper(this);
     }
@@ -48,6 +55,7 @@ public class AdminMenu extends AppCompatActivity implements View.OnClickListener
         String surname = etSurName.getText().toString();
         String lastname = etLastName.getText().toString();
         String adress = etAdress.getText().toString();
+        String id = etId.getText().toString();
 
         SQLiteDatabase database = dbHelper.getWritableDatabase();
 
@@ -77,13 +85,34 @@ public class AdminMenu extends AppCompatActivity implements View.OnClickListener
                     int surnameIndex = cursor.getColumnIndex(DBHelper.KEY_SURNAME);
                     int lastnameIndex = cursor.getColumnIndex(DBHelper.KEY_LASTNAME);
                     int adressIndex = cursor.getColumnIndex(DBHelper.KEY_ADRESS);
+
                     do {
+                        if (!id.isEmpty()) {
+                            Integer DataId = cursor.getInt(idIndex);
+                            Integer FindId = 0;
+                            try {
+                                FindId = Integer.valueOf(id);
+                            } catch (NumberFormatException e) {
+                                FindId = -1;
+                            }
+                            if (DataId == FindId) {
+                                Log.e("DataId = FindId", "Found");
+                                etIIN.setText(cursor.getString(iinIndex));
+                                etName.setText(cursor.getString(nameIndex));
+                                etSurName.setText(cursor.getString(surnameIndex));
+                                etLastName.setText(cursor.getString(lastnameIndex));
+                                etAdress.setText(cursor.getString(adressIndex));
+                            }
+
+                        }
                         Log.d("mLog", "ID = " + cursor.getInt(idIndex) +
                                 ", iin = " + cursor.getString(iinIndex) +
                                 ", name = " + cursor.getString(nameIndex) +
                                 ", surname = " + cursor.getString(surnameIndex) +
                                 ", lastname = " + cursor.getString(lastnameIndex) +
                                 ", adress = " + cursor.getString(adressIndex));
+
+
                     } while (cursor.moveToNext());
                 } else
                     Log.d("mLog", "0 rows");
@@ -93,6 +122,31 @@ public class AdminMenu extends AppCompatActivity implements View.OnClickListener
 
             case R.id.btnClear:
                 database.delete(DBHelper.TABLE_CONTACTS, null, null);
+                break;
+
+            case R.id.btnUpd:
+                if (id.equalsIgnoreCase("")) {
+                    break;
+                }
+                if (iin != " " && iin != "") contentValues.put(DBHelper.KEY_IIN, iin);
+                if (name != " " && name != "") contentValues.put(DBHelper.KEY_NAME, name);
+                if (surname != " " && surname != "")
+                    contentValues.put(DBHelper.KEY_SURNAME, surname);
+                if (lastname != " " && lastname != "")
+                    contentValues.put(DBHelper.KEY_LASTNAME, lastname);
+                if (adress != " " && adress != "") contentValues.put(DBHelper.KEY_ADRESS, adress);
+                int updCount = database.update(DBHelper.TABLE_CONTACTS, contentValues, DBHelper.KEY_ID + "= ?", new String[]{id});
+
+                Log.d("mLog", "updates rows count = " + updCount);
+                break;
+
+            case R.id.btnDel:
+                if (id.equalsIgnoreCase("")) {
+                    break;
+                }
+                int delCount = database.delete(DBHelper.TABLE_CONTACTS, DBHelper.KEY_ID + "=" + id, null);
+
+                Log.d("mLog", "deleted rows count = " + delCount);
                 break;
         }
         dbHelper.close();
